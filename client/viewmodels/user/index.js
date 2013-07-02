dd1e9b55d8db75b7b636bd8bdba1a113e64ea448
thelinuxlich <thelinuxlich@gmail.com>
2013-07-02 14:55:35 -0300

    Small fix

--- client/viewmodels/user/index.js
+++ client/viewmodels/user/index.js
@@ -11,7 +11,7 @@ function (ko,app,Table) {
                 var table = new Table(r,{'#': 'id','Nome': 'name','E-mail': 'email',
                 'Nível': 'role_name','Último Login': 'last_login_at',
                 'Data de Criação': 'created_at','Status': 'status'},["Excluir"]);
-                that.table = table.html;
+                that.table(table.html);
             });
         }
     };
